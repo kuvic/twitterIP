a785829cabc906813c5dd59dde8c5ca5c443ccae
sotiris <sotkouv@gmail.com>
2015-10-05 23:57:37 +0300

    modified:   tweetip.py

--- tweetip.py
+++ tweetip.py
@@ -12,41 +12,64 @@ import os.path
 import sys
 import socket
 
-homedir = os.path.expanduser('~')
-twitteripdir = '.twitterip'
-path = os.path.join(homedir,twitteripdir)
+def tweet(hostname = ''):
+    '''Just the hostname as argument.
+    If called from bash it can be called without arguments'''    
+    # SETTINGS - location of settings files
+    homedir = os.path.expanduser('~')
+    twitteripdir = '.twitterip'
+    path = os.path.join(homedir,twitteripdir)
+    
+    if hostname == '':
+        # set hostname from host
+        hostname = socket.gethostname()
+    
+    # check and get data from settings files
+    try:
+        with open(os.path.join(path,'APP_KEY'),'r') as f:
+            APP_KEY = f.read().strip()  # Customer Key here
+        with open(os.path.join(path,'APP_SECRET'),'r') as f:
+            APP_SECRET = f.read().strip()  # Customer Key here
+        with open(os.path.join(path,'OAUTH_TOKEN'),'r') as f:
+            OAUTH_TOKEN = f.read().strip()  # Customer Key here
+        with open(os.path.join(path,'OAUTH_TOKEN_SECRET'),'r') as f:
+            OAUTH_TOKEN_SECRET = f.read().strip()  # Customer Key here
+        with open(os.path.join(path,'ipsite_url'),'r') as f:
+            getipSite = f.read().strip()
+    
+    # if not exit with code 1
+    except:
+        print ("EXITING! Settings files not found or not readable.")
+        print ("Make sure APP_KEY and others exist in",path)
+        return 1
+    
+    # make twitter object
+    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    
+    # get ip address of router using external site PHP code
+    getipRequest = ur.Request(getipSite)
+    getipResponse = ur.urlopen(getipRequest)
+    getip = (getipResponse.read().decode('utf-8'))
+    
+    # get time from system
+    getTime = int(time())
 
-try:
-    with open(os.path.join(path,'APP_KEY'),'r') as f:
-        APP_KEY = f.read().strip()  # Customer Key here
-    with open(os.path.join(path,'APP_SECRET'),'r') as f:
-        APP_SECRET = f.read().strip()  # Customer Key here
-    with open(os.path.join(path,'OAUTH_TOKEN'),'r') as f:
-        OAUTH_TOKEN = f.read().strip()  # Customer Key here
-    with open(os.path.join(path,'OAUTH_TOKEN_SECRET'),'r') as f:
-        OAUTH_TOKEN_SECRET = f.read().strip()  # Customer Key here
-    with open(os.path.join(path,'ipsite_url'),'r') as f:
-        getipSite = f.read().strip()
+    # compose tweet text
+    tweet_text = hostname + ":" + getip + ":" + str(getTime)
+    
+    # actually tweet at last
+    twitter.update_status(status=tweet_text)
+    return 0
 
-except:
-    print ("EXITING! Settings files not found or not readable.")
-    print ("Make sure APP_KEY and others exist in",path)
-    sys.exit(1)
+if __name__ == "__main__":
+    # if call with arguments then get hostname from arguments
+    if len(sys.argv)>1:
+        host = sys.argv[1]
+    # else get hostname from system
+    else: 
+        host = ''
 
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    # tweet it - object evaluated - tweeting and returning!
+    if tweet(host) == 1:
+        sys.exit(1)
 
-getipRequest = ur.Request(getipSite)
-getipResponse = ur.urlopen(getipRequest)
-getip = (getipResponse.read().decode('utf-8'))
-
-getTime = int(time())
-if len(sys.argv)>1:
-    hostname = sys.argv[1]
-else: 
-    hostname = socket.gethostname()
-
-tweet = hostname + ":" + getip + ":" + str(getTime)
-
-# print (tweet)
-
-twitter.update_status(status=tweet)
